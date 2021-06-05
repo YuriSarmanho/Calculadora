8f06cffb618a6209c6a4943ecd7d807ec07f5808
Kae Uchoa <kaeuchoas@gmail.com>
2021-06-05 17:31:40 -0300

    refatora o codigo em funções menores e cria variavel de estado

--- app.js
+++ app.js
@@ -1,96 +1,146 @@
-let clickedValeus = []
+// indentificar outros casos de refatoração pra função
 
-let operations = []
+let clickedValeus = [];
 
-const operationStrings = ['+','-','X','/','ac','+-','%','=']
+const operationStrings = ["+", "-", "X", "/", "ac", "+-", "%", "="];
 
-let firstValue = []
-let secondValue = []
+//State
+const memory = {
+    result: 0,
+    firstValue: [],
+    secondValue: [],
+    operations: [],
+    isTypingSecondValue: false,
+    isTypingChange: false,
+    nextOperation: false,
+    countOperations: 0,
+};
 
+// Regra de negócio
+// funcionalidades
+// soma, subtração, guardar um historico das operações
 
-var isTypingSecondValue = 0
-var isTypingChange = false
-var nextOperation = false
-let changeValue = []
+// responsabilidade
+// função tem que ter uma única responsabilidade -> (ideal)
 
-function inputHandler(value){
-    const display = document.getElementById('visor')
-    display.value += value
-    const isOperation  = operationStrings.includes(value) 
-    if(isOperation){
-        operations.push(value)
-        
-        if(firstValue.length > 0){
-            //clickedValeus.push(firstValue)
-            isTypingSecondValue++
-            isTypingChange = true
-        }  
+// um código já escrito -> melhorar = refatoração(refactor)
+
+// refatora -> testa -> salva (commit)
+
+//  ÚNICA RESPONSABILIDADE
+function updateVisor(value, reset) {
+    const display = document.getElementById("visor");
+    if (reset) {
+        display.value = value;
+    } else {
+        display.value += value;
+    }
+}
+
+// retorna true or false
+function isOperation(value) {
+    return operationStrings.includes(value);
+}
+
+function isExecutingEqual(value) {
+    return memory.isTypingSecondValue && value === "=";
+}
+
+function saveOperation(value) {
+    memory.operations.push(value);
+}
+
+function cleanMemory() {
+    memory.firstValue = [];
+    memory.secondValue = [];
+    memory.operations = [];
+    memory.countOperations = 0;
+    memory.isTypingChange = false;
+    memory.nextOperation = false;
+    memory.isTypingSecondValue = false;
+}
+
+function inputHandler(value) {
+    if (isOperation(value)) {
+        saveOperation(value);
+        // define isTypingSecondValue
+        if (memory.firstValue.length > 0) {
+            memory.countOperations++;
+            memory.isTypingSecondValue = true;
+            memory.isTypingChange = true;
+        }
         //Operação simples
-        if(isTypingSecondValue > 0 && value === '='){
-            //clickedValeus.push(secondValue)
-            const result = executeOperation()
-            display.value = result 
-            isTypingSecondValue++
-            nextOperation = true
-            changeValue = result
+        if (isExecutingEqual(value)) {
+            memory.result = executeOperation();
+            updateVisor(memory.result, true);
+            memory.countOperations++;
+            memory.nextOperation = true;
         }
         //Realizar operação composta
-        if( isTypingSecondValue > 0 && operations[1] != '=' && secondValue > 0){
-            const result = executeOperation()
-            firstValue = result
-            secondValue = []
-            operations[0] = operations[1]
-            operations.pop([1])
-            nextOperation = false
-        }   
-
-         if(isTypingChange && value === '+-'){
-            changeValue = changeValue*-1
-            display.value = changeValue
-            nextOperation = false
-            secondValue= []
-            operations=[]
-            firstValue = changeValue
+        if (
+            memory.countOperations > 0 &&
+            memory.operations[1] != "=" &&
+            memory.secondValue > 0
+        ) {
+            const result = executeOperation();
+            memory.firstValue = result;
+            memory.secondValue = [];
+            memory.operations[0] = memory.operations[1];
+            memory.operations.pop([1]);
+            memory.nextOperation = false;
         }
 
-        if(value === 'ac' && firstValue.length > 0){
-            console.log('entrou')
-            display.value = null 
-            firstValue = []
-            secondValue = []
-            operations = []
-            isTypingSecondValue = 0
-            isTypingChange = false
-            nextOperation = false 
-        }  
+        if (memory.isTypingChange && value === "+-") {
+            memory.result *= -1; //memory.result = memory.result * -1
+            updateVisor(memory.result, true);
+            memory.nextOperation = false;
+            memory.secondValue = [];
+            memory.operations = [];
+            memory.firstValue = memory.result;
+        }
+
+        if (value === "ac") {
+            updateVisor(null, true);
+            cleanMemory();
+        }
         //Operação composta apertando igual
-        if(isTypingSecondValue > 1 && value != '=' && nextOperation){ 
-            const result = executeOperation()
-            operations = [operations[2]]
-            firstValue = [result]
-            secondValue = []
+        if (
+            memory.countOperations > 1 &&
+            value != "=" &&
+            memory.nextOperation
+        ) {
+            const result = executeOperation();
+            memory.operations = [memory.operations[2]];
+            memory.firstValue = [result];
+            memory.secondValue = [];
         }
-
-    } else{
-        if(isTypingSecondValue){
-            secondValue += value
-        }else{
-            firstValue += value 
+    } else {
+        updateVisor(value);
+        if (memory.isTypingSecondValue) {
+            if (memory.secondValue.length === 0) {
+                updateVisor(value, true);
+            }
+            memory.secondValue += value;
+        } else {
+            memory.firstValue += value;
         }
     }
 }
-function executeOperation(){
-    const firstOperation = operations[0]
-    switch(firstOperation){
-        case '+':
-            return Number(firstValue) + Number(secondValue)
-        case '-':
-            return Number(firstValue) - Number(secondValue)
-        case 'X':
-            return Number(firstValue)*Number(secondValue)
-        case '/':
-            return Number(firstValue)/Number(secondValue)
-        case '%':
-            return (Number(firstValue)/100) * Number(secondValue)
-        }       
+
+function executeOperation() {
+    const firstOperation = memory.operations[0];
+    switch (firstOperation) {
+        case "+":
+            return Number(memory.firstValue) + Number(memory.secondValue);
+        case "-":
+            return Number(memory.firstValue) - Number(memory.secondValue);
+        case "X":
+            return Number(memory.firstValue) * Number(memory.secondValue);
+        case "/":
+            return Number(memory.firstValue) / Number(memory.secondValue);
+        case "%":
+            return (
+                (Number(memory.firstValue) / 100) * Number(memory.secondValue)
+            );
+    }
 }
