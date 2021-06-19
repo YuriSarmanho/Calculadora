e54852346e3b7f122681acc74d640227400bd36e
YuriSarmanho <yuri.sarmanho@hotmail.com>
2021-06-19 16:38:58 -0300

    add return false

--- src/OperationHelper.js
+++ src/OperationHelper.js
@@ -9,11 +9,12 @@ class OperationHelper {
         //  tentar modificar como validar se é uma operação ou não
         //  if/else, loop for, loop while, switch
         //  return this.operationStrings.includes(value);
-         for(var itensOperation=0; itensOperation < this.operationStrings.length;itensOperation++){
-             if(value === this.operationStrings[itensOperation]){
-                 return true 
-             }
-         } 
+        for(var itensOperation=0; itensOperation < this.operationStrings.length;itensOperation++){
+            if(value === this.operationStrings[itensOperation]){
+                return true 
+            } 
+        } 
+        return false
     }
 
     isSimpleOperation(value, isTypingSecondValue) {
